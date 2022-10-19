3c33a7c034984fd4f88579656ab7a95646795794
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-19 20:52:06 -0300

    Add colisions

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,22 @@
 extends KinematicBody2D
 
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	# :delta: frame rate (1/60)
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	# Normalize distance when moving diagonally on the cartesian plan
+	input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity) 
